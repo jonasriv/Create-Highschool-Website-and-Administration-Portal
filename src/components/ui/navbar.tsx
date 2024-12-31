d9850b4a8884ecee3c7b4b90aa260793ca90f688
jonasriv <jonasriv@gmail.com>
2024-12-31 11:03:13 +0100

    vercel9

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -23,7 +23,7 @@ const Navbar: React.FC<NavbarProps> = ({isOpen, onClose }) => {
     return (
         
         <div className={`${isOpen ? "opacity-100 lg:opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-all duration-300 z-200 h-full md:h-auto `}>
-            <div className="flex fixed bg-black/70 flex-col w-full h-full lg:h-44 text-slate-100 z-200 justify-start backdrop-blur-lg">
+            <div className="flex fixed bg-black/60 flex-col w-full h-full lg:h-44 text-slate-100 z-200 justify-start backdrop-blur-lg">
                 <ul className="flex cursor-pointer flex-col lg:flex-row text-xxl font-bold lg:justify-center lg:left-0 lg:right-0 lg:mx-auto lg:max-w-screen-2xl z-200 divide-y-2 md:divide-y-0 divide-redpink ">
                     <li onClick={() => { scrollToSection("intro"); onClose(); }} className="flex hover:bg-gradient-to-r from-pinky via-redpink to-orangy items-center justify-between py-8 px-2 text-2xl lg:bg-black lg:rounded-3xl lg:m-4 lg:p-6 md:ring-2 ring-redpink"><span>Elev på Create?</span><CircleArrowRight size={42} className="pr-2 lg:hidden"/></li>
                     <li onClick={() => { scrollToSection("study"); onClose(); }} className="flex hover:bg-gradient-to-r from-pinky via-redpink to-orangy items-center justify-between py-8 px-2 text-2xl lg:bg-black lg:rounded-3xl lg:m-4 lg:p-6 md:ring-2 md:ring-redpink"><span>Musikk, dans og drama</span><CircleArrowRight  size={42} className="pr-2 lg:hidden"/></li>
