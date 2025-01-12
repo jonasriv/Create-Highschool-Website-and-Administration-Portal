c855f2853d3d8e7118e5098a59c5e103cc447620
jonasriv <jonasriv@gmail.com>
2025-01-12 20:17:23 +0100

    fixed contact8

--- src/app/Contact.jsx
+++ src/app/Contact.jsx
@@ -10,18 +10,18 @@ const Contact = () => {
             <div className="flex flex-col justify-between items-center md:items-start w-full max-w-screen-lg h-full gap-12">
                 <h1 className="text-3xl font-mina font-black">Kontakt</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 h-full w-full px-4">
-                <div className="bg-black/40 mx-4 md:mx-0 rounded-xl p-8 font-mina">
+                <div className="bg-black/40 mx-4 md:mx-0 flex flex-col rounded-xl p-8 font-mina gap-4">
                     <h1 className="text-pinky text-center">
                         Kontakt oss:
                     </h1>
-                    <div className="font-mina text-xl">
-                        <a href="mailto:mail@create.no" target="_blank" className="hover:bg-pinky rounded-xl p-2 flex flex-row">
+                    <div className="font-mina text-xl p-2 hover:bg-pinky rounded-xl">
+                        <a href="mailto:mail@create.no" target="_blank" className="flex flex-row gap-4">
                             <Mail color="white" size={22} />
                             <p>mail@create.no</p>
                         </a>
                     </div>
-                    <div className="font-mina text-xl">    
-                        <a href="tel:+4746787000"  target="_blank" className="hover:bg-pinky rounded-xl p-2 flex flex-row">
+                    <div className="font-mina text-xl p-2 hover:bg-pinky rounded-xl">    
+                        <a href="tel:+4746787000"  target="_blank" className="flex flex-row gap-4">
                             <Phone color="white" size={22} />
                             <p>+47 467 87 000</p>
                         </a>
@@ -32,16 +32,16 @@ const Contact = () => {
                     <h1 className="text-pinky text-center">
                         Følg oss:
                     </h1>
-                    <div className="font-mina text-xl flex flex-row w-full">
-                    <a href="https://www.instagram.com/createvgs/" target="_blank" className="hover:bg-pinky rounded-xl p-2 flex flex-row gap-4">
+                    <div className="font-mina text-xl p-2 hover:bg-pinky rounded-xl">
+                    <a href="https://www.instagram.com/createvgs/" target="_blank" className="flex flex-row gap-4">
                         <Instagram color="white" size={22} />
                         <p>
                             Instagram
                         </p>
                     </a>
                     </div>
-                    <div className="font-mina text-xl flex flex-row w-full">
-                    <a href="https://www.facebook.com/createvgs/" target="_blank" className="hover:bg-pinky rounded-xl p-2 flex flex-row gap-4">
+                    <div className="font-mina text-xl p-2 hover:bg-pinky rounded-xl">
+                    <a href="https://www.facebook.com/createvgs/" target="_blank" className="flex flex-row gap-4">
                         <Facebook color="white" size={22} />
                         <p>
                             Facebook
@@ -64,20 +64,20 @@ const Contact = () => {
                         2615 Lillehammer
                     </p>
                 </div>
-                <div className="bg-black/40 mx-4 md:mx-0 rounded-xl p-8 font-mina mb-8">
+                <div className="bg-black/40 mx-4 md:mx-0 rounded-xl p-8 font-mina mb-8 flex flex-col gap-2">
                     <h1 className="text-pinky text-center">
                         Les mer:
                     </h1>
-                    <a target="_blank" href="/publicfiles/ordensreglement.pdf">
+                    <a className="hover:bg-pinky p-2 rounded-xl" target="_blank" href="/publicfiles/ordensreglement.pdf">
                         <p className="font-mina text-xl">Ordensreglement</p>
                     </a>
-                    <a target="_blank" href="/publicfiles/inntaksreglement.pdf">
+                    <a className="hover:bg-pinky p-2 rounded-xl" target="_blank" href="/publicfiles/inntaksreglement.pdf">
                         <p className="font-mina text-xl">Inntaksreglement</p>
                     </a>
-                    <a target="_blank" href="/publicfiles/CREATE_-_fag_og_timefordeling_2019 (1).pdf">
+                    <a className="hover:bg-pinky p-2 rounded-xl" target="_blank" href="/publicfiles/CREATE_-_fag_og_timefordeling_2019 (1).pdf">
                         <p className="font-mina text-xl">Fag- og timefordeling</p>
                     </a>
-                    <a target="_blank" href="/publicfiles/Creates_læreplan__generell_del (1).pdf">
+                    <a className="hover:bg-pinky p-2 rounded-xl" target="_blank" href="/publicfiles/Creates_læreplan__generell_del (1).pdf">
                         <p className="font-mina text-xl">Læreplan og godkjennelse</p>
                     </a>
                 </div>
