cbdf56d109425de1bb4613450a846097d29356fd
jonasriv <jonasriv@gmail.com>
2025-01-17 14:17:32 +0100

    complete9

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -32,7 +32,7 @@ function Calendar({
           month: "space-y-4",
           caption: "flex justify-center pt-1 relative items-center",
           caption_label: "text-sm font-medium",
-          nav: "space-x-1 flex items-center",
+          nav: "space-x-1 flex items-start center",
           nav_button: cn(
             buttonVariants({ variant: "outline" }),
             "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
@@ -40,9 +40,9 @@ function Calendar({
           nav_button_previous: "absolute left-1",
           nav_button_next: "absolute right-1",
           table: "w-full border-collapse space-y-1",
-          head_row: "flex",
+          head_row: "hidden",
           head_cell:
-            "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
+            "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem] hidden",
           row: "flex w-full mt-2",
           cell: cn(
             "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
