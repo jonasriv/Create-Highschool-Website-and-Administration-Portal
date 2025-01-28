57bf4850ed632c28960d657ef8e6afe015042082
jonasriv <jonasriv@gmail.com>
2025-01-28 15:13:39 +0100

    added new montors

--- src/app/Mentorer.tsx
+++ src/app/Mentorer.tsx
@@ -7,6 +7,8 @@ const Mentorer = () => {
     const onklP = 'https://res.cloudinary.com/dtg4y0rod/image/upload/v1738063126/mentor-onklp_ijov8y.jpg';
     const makosir = 'https://res.cloudinary.com/dtg4y0rod/image/upload/v1738063269/Makosir_WMG2_qqtg0h.jpg';
     const mathilde = 'https://res.cloudinary.com/dtg4y0rod/image/upload/v1738063340/storm_ubxp3f.png';
+    const synne = 'https://res.cloudinary.com/dtg4y0rod/image/upload/v1738072918/Synnevo_Presseshoot_Robinboee-5680_y5oxzi.jpg';
+    const polsa = 'https://res.cloudinary.com/dtg4y0rod/image/upload/v1738072911/mentor-polsa_hazosj.png';
     
 
     return (
@@ -15,10 +17,10 @@ const Mentorer = () => {
             className="flex flex-col justify-center items-center w-screen max-w-screen h-auto"
         >          
             <div className="w-screen h-full gap-20 flex flex-col justify-center items-center max-w-screen pt-16">
-                <div className="flex flex-col justify-center items-center w-full px-4 font-black gap-2 break-keep">
+                <div className="flex flex-col justify-center items-center w-full px-4 font-black gap-2 mt-4 break-keep">
                     <h1 className="pb-4 text-2xl md:text-4xl font-mina">Noen av våre mentorer:</h1>
-                    <div className="w-auto h-auto max-w-screen-sm lg:max-w-screen-md grid grid-cols-2 md:grid-cols-3 gap-2 justify-center items-center rounded-3xl">
-                        <div className="h-48 w-42 md:h-56 md:w-56 rounded-none flex justify-center items-end"
+                    <div className="w-auto h-auto max-w-screen-sm lg:max-w-screen-lg grid grid-cols-2 md:grid-cols-4 gap-4 ">
+                        <div className="h-48 w-42 lg:h-56 lg:w-56 rounded-none flex justify-center items-end"
                             style={{
                                 backgroundImage: `url(${mona})`,
                                 backgroundSize: 'cover',
@@ -28,7 +30,7 @@ const Mentorer = () => {
                         >
                             <p className="font-mina w-full min-w-full bg-white/50 text-black text-lg md:text-xl min-h-8 md:min-h-12 rounded-b-none text-center flex items-center justify-center">Mona Berntsen</p>
                         </div>
-                        <div  className="h-48 w-42 md:h-56 md:w-56 rounded-none flex justify-center items-end"
+                        <div  className="h-48 w-42 lg:h-56 lg:w-56 rounded-none flex justify-center items-end"
                             style={{
                                 backgroundImage: `url(${ellen})`,
                                 backgroundSize: 'cover',
@@ -37,7 +39,7 @@ const Mentorer = () => {
                             }}>
                             <p className="font-mina w-full min-w-full bg-white/50 text-black text-lg md:text-xl min-h-8 md:min-h-12 rounded-b-none text-center flex items-center justify-center">Ellen Andrea Wang</p>
                         </div>
-                        <div className="h-48 w-42 md:h-56 md:w-56 rounded-none flex justify-center items-end"
+                        <div className="h-48 w-42 lg:h-56 lg:w-56 rounded-none flex justify-center items-end"
                             style={{
                                 backgroundImage: `url(${onklP})`,
                                 backgroundSize: 'cover',
@@ -46,7 +48,7 @@ const Mentorer = () => {
                             }}>
                             <p className="font-mina w-full min-w-full bg-white/50 text-black text-lg md:text-xl min-h-8 md:min-h-12 rounded-b-none text-center flex items-center justify-center">Onkl P</p>
                         </div>
-                        <div className="h-48 w-42 md:h-56 md:w-56 rounded-none flex justify-center items-end"
+                        <div className="h-48 w-42 lg:h-56 lg:w-56 rounded-none flex justify-center items-end"
                             style={{
                                 backgroundImage: `url(${makosir})`,
                                 backgroundSize: 'cover',
@@ -55,7 +57,7 @@ const Mentorer = () => {
                             }}                        >
                             <p className="font-mina w-full min-w-full bg-white/50 text-black text-lg md:text-xl min-h-8 md:min-h-12 rounded-b-none text-center flex items-center justify-center">Makosir</p>
                         </div>
-                        <div className="h-48 w-42 md:h-56 md:w-56 rounded-none flex justify-center items-end"
+                        <div className="h-48 w-42 lg:h-56 lg:w-56 rounded-none flex justify-center items-end"
                             style={{
                                 backgroundImage: `url(${rolf})`,
                                 backgroundSize: 'cover',
@@ -64,7 +66,7 @@ const Mentorer = () => {
                             }}                        >
                             <p className="font-mina w-full min-w-full bg-white/50 text-black text-lg md:text-xl min-h-8 md:min-h-12 rounded-b-none text-center flex items-center justify-center">Rolf Kristian Larsen</p>
                         </div>
-                        <div className="h-48 w-42 md:h-56 md:w-56 rounded-none flex justify-center items-end"
+                        <div className="h-48 w-42 lg:h-56 lg:w-56 rounded-none flex justify-center items-end"
                             style={{
                                 backgroundImage: `url(${mathilde})`,
                                 backgroundSize: 'cover',
@@ -73,6 +75,24 @@ const Mentorer = () => {
                             }}                        >
                             <p className="font-mina w-full min-w-full bg-white/50 text-black text-lg md:text-xl min-h-8 md:min-h-12 rounded-b-none text-center flex items-center justify-center">Mathilde Storm</p>
                         </div> 
+                        <div className="h-48 w-42 lg:h-56 lg:w-56 rounded-none flex justify-center items-end"
+                            style={{
+                                backgroundImage: `url(${synne})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'top',
+                                backgroundRepeat: 'no-repeat'
+                            }}                        >
+                            <p className="font-mina w-full min-w-full bg-white/50 text-black text-lg md:text-xl min-h-8 md:min-h-12 rounded-b-none text-center flex items-center justify-center">Synne Vo</p>
+                        </div> 
+                        <div className="h-48 w-42 lg:h-56 lg:w-56 rounded-none flex justify-center items-end"
+                            style={{
+                                backgroundImage: `url(${polsa})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'top',
+                                backgroundRepeat: 'no-repeat'
+                            }}                        >
+                            <p className="font-mina w-full min-w-full bg-white/50 text-black text-lg md:text-xl min-h-8 md:min-h-12 rounded-b-none text-center flex items-center justify-center">Øystein Pettersen</p>
+                        </div>                                                 
                     </div>                       
                 </div>
 
