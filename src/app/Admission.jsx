1ecbf97f21836f0e32bb2e986ff3170967aec68e
jonasriv <jonasriv@gmail.com>
2025-01-29 14:50:54 +0100

    fixed sideways again3

--- src/app/Admission.jsx
+++ src/app/Admission.jsx
@@ -24,7 +24,7 @@ const Admission = () => {
                             <TabsContent value="admission_tab" className="md:rounded-2xl w-full h-auto overflow-y-scroll">
                                 <div className="flex flex-col md:flex-row md:justify-between items-start md:pt-4">
                                     <div className="w-full pr-4 font-roboto-mono">
-                                        <div className="font-roboto text-lg md:text-2xl text-slate-300 leading-normal px-6 lg:px-16 pb-12 flex flex-col justify-center items-center">
+                                        <div className="font-roboto text-lg md:text-2xl text-slate-300 leading-normal px-6 lg:px-16 pb-12 flex flex-col justify-center items-center max-w-[1024px]">
                                             <GetTextContent contentKey="opptak" className="font-roboto-mono"/>
                                             <div className="w-full flex items-center justify-center pt-8 flex-col">
                                                 <img src={cityLife} alt="" className="w-full flex text-center h-56 md:h-80 object-cover mb-8 rounded-xl"></img>
@@ -41,7 +41,7 @@ const Admission = () => {
                             <TabsContent value="future_tab" className="md:rounded-2xl w-full h-auto overflow-y-scroll pb-12">
                                 <div className="flex flex-col md:flex-row md:justify-between items-start md:pt-4">
                                     <div className="w-full pr-4">
-                                        <div className="font-roboto text-lg md:text-2xl text-slate-300 leading-normal px-6 lg:px-16 pb-12">
+                                        <div className="font-roboto text-lg md:text-2xl text-slate-300 leading-normal px-6 lg:px-16 pb-12 max-w-[1024px]">
                                             <GetTextContent contentKey="hva_blir_jeg" />
                                             <div className="w-full flex items-center justify-center pt-8">
                                                 <Link href="/soknad">
@@ -56,7 +56,7 @@ const Admission = () => {
                             <TabsContent value="about_tab" className="md:rounded-2xl w-full h-auto overflow-y-scroll">
                                 <div className="flex flex-col md:flex-row md:justify-between items-start mb-4 ">
                                     <div className="w-full pr-4">
-                                        <div className="font-roboto text-lg md:text-2xl text-slate-300 leading-normal px-6 lg:px-16">
+                                        <div className="font-roboto text-lg md:text-2xl text-slate-300 leading-normal px-6 lg:px-16 max-w-[1024px]">
                                             <GetTextContent contentKey="om_create" />
                                             <div className="w-full flex items-center justify-center pt-8">
                                                 <Link href="/soknad">
