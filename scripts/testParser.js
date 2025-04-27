3654c6dc86429ab798ff4e649a70fb7e42c85945
jonasriv <jonasriv@gmail.com>
2025-04-27 19:22:36 +0200

    Delete scripts directory

--- scripts/testParser.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const csv = require("csv-parser");
-
-fs.createReadStream("importApplicationsFeb2025.csv")
-  .pipe(csv({ separator: ";" })) // Sjekk at separatoren er riktig!
-  .on("data", (row) => console.log("Parsed row:", row))
-  .on("end", () => console.log("CSV-parsing fullført."));
