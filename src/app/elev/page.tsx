ae6c9ec48798ae8e16d47225e447650c0a74d046
jonasriv <jonasriv@gmail.com>
2026-01-23 14:07:40 +0100

    added /elev with feide cred.

--- src/app/elev/page.tsx
+++ src/app/elev/page.tsx
@@ -8,7 +8,7 @@ export default async function ElevPage() {
     // if (!session) redirect("/api/auth/signin?callbackUrl=/elev");
 
     return (
-        <div className="flex flex-col h-screen w-screen bg-white/40"
+        <div className="flex flex-col h-screen w-screen bg-white/40 "
             style={{
                 backgroundColor: 'black',
                 backgroundImage: `url(${internalBackground})`,
@@ -19,8 +19,11 @@ export default async function ElevPage() {
             }}  
         >
             <ElevHeader user={session?.user}/>        
-            <main className="mt-24">
-                <p>Innlogget som: {session?.user?.email ?? "ukjent"}</p>HALLO
+            <main className="mt-24 h-screen min-w-full max-w-[1200px] pt-4 flex flex-col justify-start items-center">
+                <div className="bg-black/60 backdrop-blur-sm w-10/12 h-96 rounded-md p-4">
+                    {session ? <p>Innlogget som: {session?.user?.email || ""}</p> : <p>Du må logge deg inn med feide</p>}
+                </div>
+                
             </main>
         </div>
     );
